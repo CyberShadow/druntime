0ff11bfa15b56476e55b91ed2d6244574057e519
Vladimir Panteleev <vladimir@thecybershadow.net>
2015-03-25 02:24:34 +0000

    Fix incorrect re-enabled address reporting for array struct destructions

--- src/rt/lifetime.d
+++ src/rt/lifetime.d
@@ -1325,7 +1325,11 @@ void finalize_array2(void* p, size_t size) nothrow
     debug(PRINTF) printf("rt_finalize_array2(p = %p)\n", p);
 
     TypeInfo_Struct si = void;
-    debug (VALGRIND) disableAddrReportingInRange(p[0..size]);
+    debug (VALGRIND)
+    {
+        auto block = p[0..size];
+        disableAddrReportingInRange(block);
+    }
     if(size <= 256)
     {
         si = *cast(TypeInfo_Struct*)(p + size - size_t.sizeof);
@@ -1342,7 +1346,7 @@ void finalize_array2(void* p, size_t size) nothrow
         size = *cast(size_t*)p;
         p += LARGEPREFIX;
     }
-    debug (VALGRIND) enableAddrReportingInRange(p[0..size]);
+    debug (VALGRIND) enableAddrReportingInRange(block);
 
     try
     {
